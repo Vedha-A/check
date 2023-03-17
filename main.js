972c35350f315104abbea050578e3562fa23277c
Vedha-A <85233825+Vedha-A@users.noreply.github.com>
2023-03-17 15:38:51 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,58 @@
+noseX = 0;
+noseY = 0;
+marioX = 0;
+marioY = 0;
+img = "";
+function preload() {
+	world_start = loadSound("world_start.wav");
+	setSprites();
+	MarioAnimation();
+	img = loadImage('mario.jpg');
+}
+
+function setup() {
+	canvas = createCanvas(1240, 336);
+	canvas.parent('canvas');
+	instializeInSetup(mario);
+	createCanvas(650, 400);
+	video = createCapture(VIDEO);
+	video.size(600, 300);
+
+	poseNet = ml5.poseNet(video, modelLoaded);
+	poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded() 
+{
+	console.log('Model Loaded!');
+}
+
+function draw() {
+	game();
+	if(noseX < 300)
+	{
+		marioX = marioX - 1;
+	}
+	if(noseX > 300)
+	{
+		marioX = marioX + 1;
+	}
+	if(noseY < 150)
+	{
+		marioY = marioY - 1;
+	}
+	image(img, marioX, marioY, 40, 70);
+	console.log(marioX, marioY);
+}
+function gotPoses(results)
+{
+	if(results.length > 0)
+	{
+		
+		noseX = results[0].pose.nose.X;
+		console.log(noseX);
+		noseY = results[0].pose.nose.Y;
+		console.log(noseY);
+	}
+
+}
